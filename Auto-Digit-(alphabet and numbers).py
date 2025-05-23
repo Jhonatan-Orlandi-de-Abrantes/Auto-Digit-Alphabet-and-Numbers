8f1763c94407fc445353e856cb70cbb98e70d69c
Jhonatan Orlandi de Abrantes <194443620+Jhonatan-Orlandi-de-Abrantes@users.noreply.github.com>
2025-05-23 11:27:28 -0300

    Update Auto-Digit-(alphabet and numbers).py

--- Auto-Digit-(alphabet and numbers).py
+++ Auto-Digit-(alphabet and numbers).py
@@ -14,10 +14,12 @@ def auto():
         if active:
             while active:
                 k.press("Shift")
-                clique("a") or clique("b") or clique("c") or clique("d") or clique("e") or clique("f") or clique("g") or clique("h") or clique("i") or clique("q") or clique("r") or clique("s") or clique("t") or clique("u") or clique("v") or clique("w") or clique("x") or clique("y") or clique("z")
+                clique("a") or clique("b") or clique("c") or clique("d") or clique("e") or clique("f") or clique("g") or clique("h") or clique("i") or clique("q") or clique("r") or clique("s") or clique("t") or clique("u") or clique("v") or clique("w") or clique("x") or clique("y") or clique("z") or clique("_")
                 k.release("Shift")
-                clique("0") or clique("1") or clique("2") or clique("3") or clique("4") or clique("5") or clique("6") or clique("7") or clique("8") or clique("9")
+                clique("0") or clique("1") or clique("2") or clique("3") or clique("4") or clique("5") or clique("6") or clique("7") or clique("8") or clique("9") or clique("-")
                 t.sleep(0.1)
+                clique("Tab")
+                clique("Enter")
         else:
             ()
 
